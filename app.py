25a05f333bf80db2d281a1d89477610f96b848d4
Zishan9309 <zishankhan9284@gmail.com>
2026-02-12 18:32:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ div.stFormSubmitButton > button:hover {
 # ---------------- NAVBAR CONTENT ----------------
 st.markdown("""
     <div style="position: fixed; top: 18px; left: 20px; z-index: 10000; color: white; font-weight: 800; font-family: sans-serif; font-size: 20px; letter-spacing: 1px;">
-        CRYPTO RISK ANALYZER
+        
     </div>
 """, unsafe_allow_html=True)
 
