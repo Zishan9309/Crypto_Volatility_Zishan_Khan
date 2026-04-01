4913991176db9aa812bc77baf37fb94549edf136
Zishan9309 <zishankhan9284@gmail.com>
2026-04-01 15:25:04 +0530

    Update risk_class.py

--- risk_class.py
+++ risk_class.py
@@ -52,7 +52,7 @@ def render_risk_classification(data):
     """, unsafe_allow_html=True)
 
     # --- HEADER ---
-    st.markdown("<h1 style='color:white; text-align:center;'>📊 Risk Classification</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color:#4cc9f0; text-align:center;'>📊 RISK CLASSIFICATION</h1>", unsafe_allow_html=True)
     st.write("---")
 
     # --- INTERACTIVE THRESHOLD SLIDER ---
